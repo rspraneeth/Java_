549059236a3f7d430e6ac2f4ea89c4b89d65feca
rspraneeth <rspraneeth.rsp@gmail.com>
2022-10-10 20:24:44 +0530

    _

--- Collections/ArrayListExample.java
+++ Collections/ArrayListExample.java
@@ -11,7 +11,6 @@ public class ArrayListExample {
         for (int i=0; i < al.size(); i++){
             System.out.println("Element at index "+(i+1)+" is "+al.get(i));
         }
-
         al.remove(1);
         al.remove(new Integer(5));
         for (int i=0; i < al.size(); i++){
--- Filestreams/DownloadFileUri.java
+++ Filestreams/DownloadFileUri.java
@@ -8,13 +8,14 @@ import java.net.URL;
 public class DownloadFileUri {
     public static void main(String[] args) {
         try {
-            URL url = new URL("https://filesamples.com/samples/document/txt/sample3.txt");
-            File f = new File("Downld.txt");
+            URL url = new URL("https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80");
+            File f = new File("Download.jpeg");
             FileUtils.copyURLToFile(url, f);
         } catch (MalformedURLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         } catch (IOException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
